2b0e300bae508cddbae8b8825fab37870589f37b
Marc Jacobi <Marc.Jacobi@macaw.nl>
2025-11-07 10:32:30 +0100

    tests

--- Jacobi.ValueObject.Tests/ValueObjectMethods.cs
+++ Jacobi.ValueObject.Tests/ValueObjectMethods.cs
@@ -3,6 +3,37 @@ namespace Jacobi.ValueObject.Tests;
 public class ValueObjectMethods
 {
     [Fact]
+    public void DefaultConstructor_Error()
+    {
+        var decl = """
+            [ValueObject<int>]
+            public partial record struct ValObj;
+            """;
+        var usage = """
+            var vo = new ValObj();
+            // should throw
+            """;
+
+        Generator.ExpectException<ValueObjectException>(decl, usage);
+    }
+
+    [Fact]
+    public void Uninitialized_Error()
+    {
+        var decl = """
+            [ValueObject<int>]
+            public partial record struct ValObj;
+            """;
+        var usage = """
+            var arr = new ValObj[1];
+            var val = arr[0].Value;
+            // should throw
+            """;
+
+        Generator.ExpectException<ValueObjectException>(decl, usage);
+    }
+
+    [Fact]
     public void Static_IsValid()
     {
         var decl = """
